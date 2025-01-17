d2d3f180a1a20240be7079a1302598f9e9a644df
ronitchawla01 <ronitchawla1234@gmail.com>
2025-01-17 22:43:00 +0500

    Add files via upload

---
+++ BinarySearchTree.java
@@ -0,0 +1,142 @@
+class Node1{
+    int key;
+    Node left , right ;
+    public Node1(int item)
+    {
+        key=item;
+        left=right=null;
+    }
+}
+class BinarySearchTree {
+
+    Node root;
+    BinarySearchTree(int key)
+    {
+        root = new Node(key);
+    }
+
+    BinarySearchTree()
+    {
+        root = null;
+    }
+
+    public void printPostOrder(Node node)
+    {
+        if(node==null)
+        {
+            return ;
+        }
+        printPostOrder(node.left);
+        printPostOrder(node.right);
+
+        System.out.println(node.key+" ");
+    }
+
+    public void printInOrder(Node node)
+    {
+        if(node==null)
+        {
+            return;
+        }
+
+        printInOrder(node.left);
+
+        System.out.println(node.key+" ");
+
+        printInOrder(node.right);
+    }
+
+    public void printPreOrder(Node node)
+    {
+        if(node==null)
+        {
+            return;
+        }
+
+        System.out.println(node.key+" ");
+
+        printPreOrder(node.left);
+
+        printPreOrder(node.right);
+    }
+    public static void main(String[] args) {
+
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.root = new Node(4);
+        Node a = new Node(7);
+        tree.root.left = a;
+
+        Node b = new Node(5);
+        tree.root.right = b;
+
+        a.left = new Node(8);
+        a.right = new Node(3);
+
+        b.left = new Node(4);
+        b.right = new Node(1);
+
+        System.out.println("pre-order");
+        tree.printPreOrder(tree.root);
+
+        System.out.println("post-order");
+        tree.printPostOrder(tree.root);
+
+        System.out.println("In-order");
+        tree.printInOrder(tree.root);
+
+    }
+    public Node search(Node root,int key)
+    {
+        if(root==null || root.key==key)
+            return root;
+
+        if(root.key>key)
+            return search(root.left,key);
+        return search(root.right,key);
+
+    }
+    Node insertRec(Node root,int key){
+        if(root==null){
+            root=new Node(key);
+            return root;
+        }
+        else{
+            if(key<root. key)
+                root.left=insertRec(root.left,key);
+            else if(key>root.key)
+                root.right = insertRec(root.right,key);
+        }
+        return root;
+    }
+    Node deleteRec(Node root,int key)
+    {
+        if(root==null)
+            return root;
+        if(key<root.key)
+            root.left=deleteRec(root.left,key);
+        else if(key>root.key)
+            root.right=deleteRec(root.right,key);
+        else{
+            if(root.left==null)
+                return root.right;
+            else if(root.right==null)
+                return root.left;
+
+            root.key = minValue(root.right);
+            root.right = deleteRec(root.right, root.key);
+        }
+        return root;
+    }
+    int minValue(Node root)
+    {
+        int minv = root.key;
+        while(root.left!=null)
+        {
+            minv = root.left.key;
+            root = root.left;
+        }
+        return minv;
+
+    }
+
+}
