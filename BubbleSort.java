166a607f2f209ba58d68c940b15667a443fbc833
ronitchawla01 <ronitchawla1234@gmail.com>
2025-01-19 15:02:43 +0500

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class BubbleSort {
+    public static void main(String args[]) {
+        int n = 5;
+        int[] arr = {67, 44, 82, 17, 20};
+
+        System.out.print("Array before Sorting: ");
+        for(int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+
+        for(int i = 0; i < n; i++) {
+            int swaps = 0;
+            for(int j = 0; j < n - i - 1; j++) {
+                if(arr[j] > arr[j + 1]) {
+                    int temp;
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swaps = 1;
+                }
+            }
+            if(swaps == 0)
+                break;
+        }
+
+        System.out.print("Array After Sorting: ");
+        for(int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+}
